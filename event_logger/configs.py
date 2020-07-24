25d71901a8c020073c7b7e65a60beff46b0671ad
Pritom Borogoria <pritom.saan@gmail.com>
2020-07-24 14:27:04 +0600

    ModuleConfiguration allow_sensitive_test_cases method done with the proper and complete test cases

--- event_logger/configs.py
+++ event_logger/configs.py
@@ -10,23 +10,26 @@ class ModuleConfigurations:
 
     default_allow_sensitive_test_case = False   # check self.allow_sensitive_test_cases() method
 
-    def allow_sensitive_test_cases(self, on_test=False):
+    def allow_sensitive_test_cases(self, on_test=False, test_settings_object=None):
         """The default value is FALSE. If it is TRUE then it will run some model based TestCases which may not be
            suitable for your application. If it throws any error then just make it FALSE. The second argument
-           on_test is used for various TestCases to run. Genrally it is False so the original data will run if
-           it is True then only the sent fake data will run"""
+           on_test is used for various TestCases to run. Genrally it is False so the original data will
+           run if it is True then only the sent fake data will run. The test_settings_object contains a fake
+           settings object if on_test is True"""
         if on_test is False:
-            if hasattr(settings, 'EVENT_LOGGER_SETTINGS'):
-                if 'sensitive_test_cases' in settings.EVENT_LOGGER_SETTINGS:
-                    if settings.EVENT_LOGGER_SETTINGS['sensitive_test_cases'] is True:
-                        return True
-                    elif settings.EVENT_LOGGER_SETTINGS['sensitive_test_cases'] is False:
-                        return False
-                    else:
-                        raise ValidationError('sensitive_test_cases value is expected a Boolean value')
+            config_settings_object = settings
+        else:
+            config_settings_object = test_settings_object
+
+        if hasattr(config_settings_object, 'EVENT_LOGGER_SETTINGS'):
+            if 'sensitive_test_cases' in config_settings_object.EVENT_LOGGER_SETTINGS:
+                if config_settings_object.EVENT_LOGGER_SETTINGS['sensitive_test_cases'] is True:
+                    return True
+                elif config_settings_object.EVENT_LOGGER_SETTINGS['sensitive_test_cases'] is False:
+                    return False
                 else:
-                    return self.default_allow_sensitive_test_case
+                    raise ValidationError('sensitive_test_cases value is expected a Boolean value')
             else:
                 return self.default_allow_sensitive_test_case
         else:
-            pass
+            return self.default_allow_sensitive_test_case
--- event_logger/tests/test_configs_methods.py
+++ event_logger/tests/test_configs_methods.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 from ..configs import ModuleConfigurations
 
 
@@ -7,7 +8,37 @@ class ModuleConfigurationsTestCases(TestCase):
     test_class_name = ModuleConfigurations()
 
     def setUp(self):
-        pass
+        """Constructing a fake settings object for testing"""
+        fake_settings_object_list = []
+        fake_settings_object = type('test', (object,), {})()
+        fake_settings_object_list.append(fake_settings_object)
+        #  Setting EVENT_LOGGER_SETTINGS keyword to the fake_settings_object
+        fake_settings_object = type('test', (object,), {})()
+        fake_settings_object.EVENT_LOGGER_SETTINGS = lambda: None
+        setattr(fake_settings_object.EVENT_LOGGER_SETTINGS, 'EVENT_LOGGER_SETTINGS', {})
+        fake_settings_object_list.append(fake_settings_object)
+        fake_settings_object = type('test', (object,), {})()
+        fake_settings_object.EVENT_LOGGER_SETTINGS = lambda: None
+        setattr(fake_settings_object.EVENT_LOGGER_SETTINGS, 'EVENT_LOGGER_SETTINGS', {})
+        fake_settings_object.EVENT_LOGGER_SETTINGS.EVENT_LOGGER_SETTINGS['sensitive_test_cases'] = False
+        fake_settings_object_list.append(fake_settings_object)
+        fake_settings_object = type('test', (object,), {})()
+        fake_settings_object.EVENT_LOGGER_SETTINGS = lambda: None
+        setattr(fake_settings_object.EVENT_LOGGER_SETTINGS, 'EVENT_LOGGER_SETTINGS', {})
+        fake_settings_object.EVENT_LOGGER_SETTINGS.EVENT_LOGGER_SETTINGS['sensitive_test_cases'] = True
+        fake_settings_object_list.append(fake_settings_object)
+        fake_settings_object = type('test', (object,), {})()
+        fake_settings_object.EVENT_LOGGER_SETTINGS = lambda: None
+        setattr(fake_settings_object.EVENT_LOGGER_SETTINGS, 'EVENT_LOGGER_SETTINGS', {})
+        fake_settings_object.EVENT_LOGGER_SETTINGS.EVENT_LOGGER_SETTINGS['sensitive_test_cases'] = 'Some Value'
+        fake_settings_object_list.append(fake_settings_object)
+        return fake_settings_object_list
 
     def test_allow_sensitive_test_cases_method(self):
+        test_settings_object_list = self.setUp()
         self.assertEqual(self.test_class_name.default_allow_sensitive_test_case, False)
+        self.assertEqual(self.test_class_name.allow_sensitive_test_cases(True, test_settings_object_list[0]), False)
+        self.assertEqual(self.test_class_name.allow_sensitive_test_cases(True, test_settings_object_list[1].EVENT_LOGGER_SETTINGS), False)
+        self.assertEqual(self.test_class_name.allow_sensitive_test_cases(True, test_settings_object_list[2].EVENT_LOGGER_SETTINGS), False)
+        self.assertEqual(self.test_class_name.allow_sensitive_test_cases(True, test_settings_object_list[3].EVENT_LOGGER_SETTINGS), True)
+        self.assertRaises(ValidationError, self.test_class_name.allow_sensitive_test_cases, on_test=True, test_settings_object=test_settings_object_list[4].EVENT_LOGGER_SETTINGS)
