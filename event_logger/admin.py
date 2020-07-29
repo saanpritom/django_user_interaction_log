ee57e2de6b93a2e7c11a4bcd49e4031cd8893745
Pritom Borogoria <pritom.saan@gmail.com>
2020-07-29 15:48:02 +0600

    django admin configured for the model

--- event_logger/admin.py
+++ event_logger/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import LogRecordsModel
+
 
 # Register your models here.
+class EventLoggerAdmin(admin.ModelAdmin):
+    list_display = ['user_object_id', 'log_detail', 'target_object_id', 'event_path', 'log_time']
+    search_fields = ['user_object_id', 'target_object_id']
+
+    def log_time(self, obj):
+        return str(obj.get_timesince()) + ' ago'
+
+
+admin.site.register(LogRecordsModel, EventLoggerAdmin)
