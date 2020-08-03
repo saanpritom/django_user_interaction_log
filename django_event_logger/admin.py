c607a69981551049b7a2bec7d0fb0acf1c05a8b7
Pritom Borogoria <pritom.saan@gmail.com>
2020-08-03 23:25:21 +0600

    package name changed to django_event_logger

--- django_event_logger/admin.py
+++ django_event_logger/admin.py
@@ -3,12 +3,13 @@ from .models import LogRecordsModel
 
 
 # Register your models here.
-class EventLoggerAdmin(admin.ModelAdmin):
-    list_display = ['user_object_id', 'log_detail', 'target_object_id', 'event_path', 'log_time']
+class DjangoEventLoggerAdmin(admin.ModelAdmin):
+    list_display = ['user_object_id', 'log_detail', 'target_object_id',
+                    'event_path', 'log_time']
     search_fields = ['user_object_id', 'target_object_id']
 
     def log_time(self, obj):
         return str(obj.get_timesince()) + ' ago'
 
 
-admin.site.register(LogRecordsModel, EventLoggerAdmin)
+admin.site.register(LogRecordsModel, DjangoEventLoggerAdmin)
--- django_event_logger/apps.py
+++ django_event_logger/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class EventLoggerConfig(AppConfig):
-    name = 'event_logger'
+    name = 'django_event_logger'
